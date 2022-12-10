1a2b4c655b91b414a283166bc0cdb04b47db6563
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-12-10 17:45:09 -0500

    [2.9] Code stubs for WMSServiceServer class

---
+++ src/wms_service_server.cpp
@@ -0,0 +1,70 @@
+/******************************************************************************
+ * MIT License
+
+  Copyright (c) 2022 Nitesh Jha, Tanuj Thakkar
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+* *******************************************************************************
+*/
+
+/**
+ * @copyright Copyright (c) 2022 Nitesh Jha, Tanuj Thakkar
+ *
+ * @file wms_service_server.cpp
+ * @author Nitesh Jha (Navigator), Tanuj Thakkar (Driver)
+ * @brief This file contains the class definition for the WMS service server
+ * 
+ */
+
+
+#pragma once
+
+#include <ros/ros.h>
+
+#include <Artemis/wms_service_server.hpp>
+#include <artemis_msgs/WMSTask.h>
+
+namespace Artemis {
+
+/**
+ * @brief This function is used to define the callback function for the WMS service server
+ * 
+ * @param req 
+ * @param res 
+ * @return true 
+ * @return false 
+ */
+bool WMSServiceServer::assignTask(const artemis_msgs::WMSTask::Request& req,
+                                          artemis_msgs::WMSTask::Response& res) {
+    return true;
+}
+
+/**
+ * @brief This function is used to define the constructor for the WMS service server
+ * 
+ * @param node_handle 
+ */
+WMSServiceServer::WMSServiceServer(const ros::NodeHandle& node_handle) {
+}
+
+/**
+ * @brief This function is used to define the destructor for the WMS service server
+ * 
+ */
+WMSServiceServer::~WMSServiceServer() {}
+
+}  // namespace Artemis
