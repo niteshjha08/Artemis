46a4b9a2bbc2af6b70d89de7ddac48180385a078
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-12-10 18:01:09 -0500

    [2.10] Code stubs for WMSServiceClient class

---
+++ include/Artemis/wms_service_client.hpp
@@ -0,0 +1,74 @@
+/******************************************************************************
+ * MIT License
+
+  Copyright (c) 2022 Nitesh Jha, Tanuj Thakkar
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+* *******************************************************************************
+*/
+
+/**
+ * @copyright Copyright (c) 2022 Nitesh Jha, Tanuj Thakkar
+ *
+ * @file wms_service_client.hpp
+ * @author Nitesh Jha (Navigator), Tanuj Thakkar (Driver)
+ * @brief This file contains the class declaration for the WMS service client
+ *
+ */
+
+#pragma once
+
+#include <artemis_msgs/WMSTask.h>
+#include <ros/ros.h>
+
+namespace artemis {
+
+/**
+ * @brief This class is used to communicate with the WMS service server
+ *
+ */
+class WMSServiceClient {
+ public:
+  /**
+   * @brief Construct a new WMSServiceClient object
+   *
+   * @param node_handle The ROS node handle
+   */
+  WMSServiceClient(const ros::NodeHandle& node_handle);
+
+  /**
+   * @brief Destroy the WMSServiceClient object
+   *
+   */
+  virtual ~WMSServiceClient();
+
+  /**
+   * @brief This function is used to send a task to the WMS service server
+   *
+   * @param task The task msg to be sent to the WMS service server
+   * @return true If the task was successfully received from the WMS service
+   * server
+   * @return false If the task was not successfully received from the WMS
+   * service server
+   */
+  bool receiveTask(const artemis_msgs::WMSTask& task);
+
+ private:
+  ros::ServiceClient wms_service_client_;  // WMS service client
+};
+
+}  // namespace artemis
---
+++ src/wms_service_client.cpp
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * MIT License
+
+  Copyright (c) 2022 Nitesh Jha, Tanuj Thakkar
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+* *******************************************************************************
+*/
+
+/**
+ * @copyright Copyright (c) 2022 Nitesh Jha, Tanuj Thakkar
+ *
+ * @file wms_service_client.cpp
+ * @author Nitesh Jha (Navigator), Tanuj Thakkar (Driver)
+ * @brief This file contains the class defintion for the WMS service client
+ *
+ */
+
+#include <Artemis/wms_service_client.hpp>
+
+namespace artemis {
+
+/**
+ * @brief Construct a new WMSServiceClient::WMSServiceClient object
+ *
+ * @param node_handle Node handle for the ROS node
+ */
+WMSServiceClient::WMSServiceClient(const ros::NodeHandle& node_handle) {}
+
+/**
+ * @brief Destroy the WMSServiceClient::WMSServiceClient object
+ *
+ */
+WMSServiceClient::~WMSServiceClient() {}
+
+/**
+ * @brief This function is used to send a task to the WMS service server
+ *
+ * @return true Received task successfully
+ * @return false Failed to receive task
+ */
+bool WMSServiceClient::recieveTask(const artemis_msgs::WMSTask& task) {
+  return true;
+}
+
+}  // namespace artemis
