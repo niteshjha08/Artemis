d730bd24cd80b33fe39c3dcb10bba629ebe5bf29
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-12-14 01:15:26 -0500

    [2.17] Implemented WMSServiceServer class

--- include/Artemis/wms_service_server.hpp
+++ include/Artemis/wms_service_server.hpp
@@ -32,8 +32,13 @@
 
 #pragma once
 
-#include <Artemis/WMSTask.h>
+#include <random>
+
 #include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseArray.h>
+
+#include <Artemis/WMSTask.h>
 
 namespace Artemis {
 
@@ -42,33 +47,39 @@ namespace Artemis {
  *
  */
 class WMSServiceServer {
- private:
-  ros::ServiceServer wms_service_server_;  // WMS service server
-
-  /**
-   * @brief This function is used to define the callback function for the WMS
-   * service server
-   *
-   * @param req
-   * @param res
-   * @return true
-   * @return false
-   */
-  bool assignTask(const Artemis::WMSTask::Request::ConstPtr req,
-                  const Artemis::WMSTask::Response::ConstPtr res);
-
  public:
   /**
    * @brief Construct a new WMS Service Server object
    *
    */
-  explicit WMSServiceServer(const ros::NodeHandle& node_handle);
+  WMSServiceServer(const ros::NodeHandle& node_handle, const std::string& service_name);
 
   /**
    * @brief Destroy the WMS Service Server object
    *
    */
   ~WMSServiceServer();
+
+  /**
+    * @brief This function is used to define the callback function for the WMS
+    * service server
+    *
+    * @param req
+    * @param res
+    * @return true
+    * @return false
+    */
+    bool assignTask(Artemis::WMSTask::Request& req,
+                    Artemis::WMSTask::Response& res);
+
+  private:
+    ros::NodeHandle node_handle_;  // Node handle
+    ros::ServiceServer wms_service_server_;  // WMS service server
+    std::vector<int> task_queue_ = {1,2,3,4};  // Task queue
+    const std::vector<std::vector<float>> staging_goals_  = {{1.750, 5.650, 0., 0., 0., -1.00, 0.},
+                                                       {-1.000, 1.950, 0., 0., 0., 0.700, 0.713},
+                                                       {-4.335, 5.365, 0., 0., 0., 0., 1.000},
+                                                       {-1.061, 9.029, 0., 0., 0., -0.700, 0.700}};  // Staging goals
 };
 
 }  // namespace Artemis
