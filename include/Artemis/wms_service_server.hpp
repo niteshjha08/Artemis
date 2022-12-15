29cf9e9ee4a76c262798f88ccfecf6e71784edc4
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-12-15 06:27:05 -0500

    Fixed task ID as per Aruco markers

--- include/Artemis/wms_service_server.hpp
+++ include/Artemis/wms_service_server.hpp
@@ -38,9 +38,9 @@
 #include <ros/ros.h>
 
 #include <random>
+#include <string>
 #include <unordered_map>
 #include <vector>
-#include <string>
 
 namespace Artemis {
 
@@ -76,10 +76,10 @@ class WMSServiceServer {
                   Artemis::WMSTask::Response& res);
 
  private:
-  ros::NodeHandle node_handle_;                 // Node handle
-  const std::string service_name_;              // Service name
-  ros::ServiceServer wms_service_server_;       // WMS service server
-  std::vector<int> task_queue_ = {581, 582, 583, 584};  // Task queue
+  ros::NodeHandle node_handle_;                         // Node handle
+  const std::string service_name_;                      // Service name
+  ros::ServiceServer wms_service_server_;               // WMS service server
+  std::vector<int> task_queue_ = {582, 581, 583, 584};  // Task queue
   const std::vector<std::vector<float>> staging_goals_ = {
       {1.750, 5.650, 0., 0., 0., -1.00, 0.},
       {-1.000, 1.950, 0., 0., 0., 0.700, 0.713},
