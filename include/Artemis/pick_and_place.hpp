01907929963523775d6d294ee63d9dfdc16ae38d
Nitesh Jha <niteshj818@gmail.com>
2022-12-15 07:13:05 -0500

    [3.5], [3.9] Stubs and implementation of Pick and Place task

---
+++ include/Artemis/pick_and_place.hpp
@@ -0,0 +1,65 @@
+/******************************************************************************
+ * MIT License
+
+  Copyright (c) 2022 Nitesh Jha, Tanuj Thakkar
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+* *******************************************************************************
+*/
+
+/**
+ * @copyright Copyright (c) 2022 Nitesh Jha, Tanuj Thakkar
+ *
+ * @file navigator.hpp
+ * @author Nitesh Jha (Driver), Tanuj Thakkar (Navigator)
+ * @brief This file contains the declaration of the PickAndPlace class
+ *
+ */
+
+#pragma once
+
+#include <ros/ros.h>
+#include <string>
+#include <std_msgs/Float64.h>
+#include <gazebo_ros_link_attacher/Attach.h>
+#include <gazebo_ros_link_attacher/AttachRequest.h>
+#include <gazebo_ros_link_attacher/AttachResponse.h>
+
+
+namespace Artemis {
+
+    class PickAndPlace {
+    public:
+    PickAndPlace(const ros::NodeHandle& node_handle,
+                   const std::string& action_name);
+
+    ~PickAndPlace();
+
+    void pickCargo(int task_id);
+
+    void placeCargo(int task_id);
+
+    private:
+        std::string action_name_;
+        ros::NodeHandle node_handle_;       // NodeHandle for the node
+        ros::Publisher shoulder_pub_;     // Publisher for shoulder joint
+        ros::Publisher elbow_pub_;        // Publisher for elbow joint
+        ros::ServiceClient attach_client_;  // Service client for attaching
+        ros::ServiceClient detach_client_;  // Service client for detaching
+};
+
+}
---
+++ src/pick_and_place.cpp
@@ -0,0 +1,116 @@
+/******************************************************************************
+ * MIT License
+
+  Copyright (c) 2022 Nitesh Jha, Tanuj Thakkar
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+* *******************************************************************************
+*/
+
+/**
+ * @copyright Copyright (c) 2022 Nitesh Jha, Tanuj Thakkar
+ *
+ * @file navigator.hpp
+ * @author Nitesh Jha (Driver), Tanuj Thakkar (Navigator)
+ * @brief This file contains the declaration of the PickAndPlace class
+ *
+ */
+
+#include <pick_and_place.hpp>
+
+namespace Artemis {
+
+    PickAndPlace::PickAndPlace(const ros::NodeHandle& node_handle,
+                    const std::string& action_name) : action_name_(action_name), node_handle_(node_handle) {
+            shoulder_pub_ = node_handle_.advertise<std_msgs::Float64>(
+        "/UR5_shoulder_lift_joint_controller/command", 10);
+        elbow_pub_ = node_handle_.advertise<std_msgs::Float64>(
+        "/UR5_shoulder_lift_joint_controller/command", 10);
+
+        attach_client_ = node_handle_.serviceClient<gazebo_ros_link_attacher::Attach>("link_attacher_node/attach");
+        detach_client_ = node_handle_.serviceClient<gazebo_ros_link_attacher::Attach>("link_attacher_node/detach");
+
+        while(!attach_client_.waitForExistence(ros::Duration(1.0))) {
+            ROS_INFO("Waiting for the attach service to come up");
+        }
+        ROS_INFO_STREAM("Attach service is up");
+                                      
+    }
+
+    PickAndPlace::~PickAndPlace() {
+    }
+
+    void PickAndPlace::pickCargo(int task_id) {
+        double angle = 0;
+        double increment = -0.1;
+        double shoulder_pick_pose = -3.3;
+        std_msgs::Float64 pubMsg;
+        while(angle > shoulder_pick_pose) {
+            // shoulder_pub_.publish(angle);
+            angle += increment;
+            pubMsg.data = angle;
+            shoulder_pub_.publish(pubMsg);
+        }
+        pubMsg.data = -0.2;
+        elbow_pub_.publish(pubMsg);
+
+        // Call attach service
+        gazebo_ros_link_attacher::Attach attach_req;
+        attach_req.request.model_name_1 = "husky";
+        attach_req.request.link_name_1 = "ur_arm_wrist_3_link";
+
+        attach_req.request.model_name_2 = std::to_string(task_id);
+        attach_req.request.link_name_2 = "link";
+        if (detach_client_.call(attach_req)) {
+            ROS_INFO_STREAM("Picked cargo successfully");
+        } else {
+            ROS_ERROR_STREAM("Failed to call attach service");
+        }
+        
+    }
+
+    void PickAndPlace::placeCargo(int task_id) {
+
+        // Call detach service
+        gazebo_ros_link_attacher::Attach detach_req;
+        detach_req.request.model_name_1 = "husky";
+        detach_req.request.link_name_1 = "ur_arm_wrist_3_link";
+
+        detach_req.request.model_name_2 = std::to_string(task_id);
+        detach_req.request.link_name_2 = "link";
+        if (detach_client_.call(detach_req)) {
+            ROS_INFO_STREAM("Placed cargo successfully");
+        } else {
+            ROS_ERROR_STREAM("Failed to call detach service");
+        }
+
+        double angle = -3.3;
+        double increment = 0.1;
+        double shoulder_place_pose = 0;
+        std_msgs::Float64 pubMsg;
+        while(angle < shoulder_place_pose) {
+            // shoulder_pub_.publish(angle);
+            angle += increment;
+            pubMsg.data = angle;
+            shoulder_pub_.publish(pubMsg);
+        }
+        pubMsg.data = 0;
+        elbow_pub_.publish(pubMsg);
+        
+    }
+}  // namespace Artemis
+
